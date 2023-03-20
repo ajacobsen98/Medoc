3b417fe11587f7035b99f17741cb309a681124ee
ajacobsen98 <121130797+ajacobsen98@users.noreply.github.com>
2023-03-20 12:37:30 +0000

    deleted frush commit upcoming

--- src/guards/guest.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-
-@Injectable()
-export class GuestGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    return !request.user; // Allow access if user is not authenticated
-  }
-}
--- src/posts.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Resolver, Query } from '@nestjs/graphql';
-
-@Resolver()
-export class PostsResolver {
-  @Query(() => String)
-  hello(): string {
-    return 'Hello, World!';
-  }
-}
