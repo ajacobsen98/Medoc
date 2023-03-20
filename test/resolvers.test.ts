bea6dc7f4541b65df9d5a350a068403bf74191bb
ajacobsen98 <121130797+ajacobsen98@users.noreply.github.com>
2023-03-20 12:35:09 +0000

    lesgo

---
+++ test/resolvers.test.ts
@@ -0,0 +1,20 @@
+import { resolvers } from '/.../src/resolvers';
+
+describe('Resolvers', () => {
+  describe('Query', () => {
+    test('products', async () => {
+      // Write test cases for the products query
+      
+    });
+
+    // Add tests for other queries
+  });
+
+  describe('Mutation', () => {
+    test('createUser', async () => {
+      // Write test cases for the createUser mutation
+    });
+
+    // Add tests for other mutations
+  });
+});
